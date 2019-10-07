5a5f068c09ec783c12cb998fba004dab182a953a
Maximo Rodriguez <maximo1225@gmail.com>
2019-10-07 13:48:17 -0400

    funcionalidad drawer menu

--- app/src/main/java/pucmm/temas/especiales/e_commerce_app/MainActivity.java
+++ app/src/main/java/pucmm/temas/especiales/e_commerce_app/MainActivity.java
@@ -1,14 +1,46 @@
 package pucmm.temas.especiales.e_commerce_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        this.mToggle = new ActionBarDrawerToggle(this, this.mDrawerLayout, R.string.open, R.string.close);
+
+        this.mDrawerLayout.addDrawerListener(this.mToggle);
+        this.mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.right_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(this.mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
