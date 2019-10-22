83b0a53a630241013fb6586bc33d0d527a7547c9
Maximo Rodriguez <maximo1225@gmail.com>
2019-10-22 13:21:57 -0400

    metodos de validacion de password y del email del usuario

--- app/src/main/java/pucmm/temas/especiales/e_commerce_app/utils/FieldValidator.java
+++ app/src/main/java/pucmm/temas/especiales/e_commerce_app/utils/FieldValidator.java
@@ -9,4 +9,26 @@ public class FieldValidator {
         String input = editText.getText().toString().trim();
         return input.length() == 0;
     }
+
+    public static boolean isPasswordValid(final EditText view, final String pass) {
+        if (pass.length() < 4 || pass.length() > 20) {
+            view.setError("Password Must consist of 4 to 20 characters");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isEmailValid(final EditText view, final String email) {
+        if (email.length() < 4 || email.length() > 30) {
+            view.setError("Email Must consist of 4 to 30 characters");
+            return false;
+        } else if (!email.matches("^[A-za-z0-9.@]+")) {
+            view.setError("Only . and @ characters allowed");
+            return false;
+        } else if (!email.contains("@") || !email.contains(".")) {
+            view.setError("Email must contain @ and .");
+            return false;
+        }
+        return true;
+    }
 }
