ad0b679619919c2e7a536d0459eaa3d2a9d36728
Maximo Rodriguez <maximo1225@gmail.com>
2019-10-29 17:04:47 -0400

    Clase MessageDialog para mostrar mensajes modificados

---
+++ app/src/main/java/pucmm/temas/especiales/e_commerce_app/dialog/MessageDialog.java
@@ -0,0 +1,27 @@
+package pucmm.temas.especiales.e_commerce_app.dialog;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+import pucmm.temas.especiales.e_commerce_app.R;
+
+public class MessageDialog {
+    private static MessageDialog instance;
+    private static AlertDialog.Builder builder;
+
+    private MessageDialog(){ }
+
+    public static MessageDialog getInstance(Context context){
+        builder = new AlertDialog.Builder(context);
+        if(instance == null){
+            instance = new MessageDialog();
+        }
+        return instance;
+    }
+
+    public void informationDialog(String message){
+        builder.setIcon(R.drawable.ic_error_alert);
+        builder.setTitle("Information Message").setMessage(message);
+        builder.create().show();
+    }
+}
